1e3d8084916143eb2caf665354da40f8d2bb42d6
kushalts <kushal.nanavati@tatvasoft.com>
2022-06-16 20:02:10 +0530

    celebrate-error-fix

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, Application } from 'express';
 import * as dotenv from 'dotenv';
 import routes from './routes';
 import * as db from './models';
+import { errors } from 'celebrate';
 import environmentConfig from './constants/environment.constant';
 dotenv.config();
 export class App {
@@ -11,6 +12,7 @@ export class App {
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(routes);
+    this.app.use(errors());
   }
   public async listen() {
     await db.sequelize.sync({ force: false });
